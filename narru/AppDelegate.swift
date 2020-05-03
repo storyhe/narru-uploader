f60dcaa4803a55465aed882ea3cd44d672271385
Hyeonwoo Park <storyhe.love@gmail.com>
2020-05-03 18:39:47 +0900

    Initial Commit

---
+++ narru/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  narru
+//
+//  Created by Hyeonwoo Park on 2020/05/03.
+//  Copyright © 2020 narru. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ narru/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  narru
+//
+//  Created by Hyeonwoo Park on 2020/05/03.
+//  Copyright © 2020 narru. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
